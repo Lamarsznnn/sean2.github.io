e89f766fdf8c4d80459e9bed3c96b40d8927ad3e
Lamarsznnn <njengasean89@gmail.com>
2024-07-14 21:04:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Adding JavaScript functionality here //
+document.querySelector('form').addEventListener('submit', (Event) => {
+    event.preventDefault();
+    alert ("form submitted succesfully");
+});
